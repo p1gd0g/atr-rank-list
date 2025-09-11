4656b88a704ce3bfc0486f7463a2dc00d929db9e
renhao.szu <renhao.szu@gmail.com>
2025-09-11 12:03:13 +0000

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
+import 'package:myapp/firebase_options.dart';
 import 'dart:developer' as developer;
 
 import 'package:stack_trace/stack_trace.dart';
@@ -9,7 +12,13 @@ const String appName = 'ATR rank list';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then((
+    x,
+  ) {
+    FirebaseAnalytics.instance.logAppOpen();
+  });
+
   runApp(
     GetMaterialApp(
       logWriterCallback: (value, {isError = false}) {
@@ -24,7 +33,6 @@ void main() {
       },
       title: appName,
       theme: FlexThemeData.light(scheme: FlexScheme.purpleBrown),
-      
     ),
   );
 }
